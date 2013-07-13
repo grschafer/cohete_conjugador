0992ef14f15f673a4f40ae81715b3a60b3be4842
Greg Schafer <grschafer@gmail.com>
2013-07-12 20:17:39 -0600

    Fix output to use accents (not encodings) and windows line endings

--- scrape_verbs.py
+++ scrape_verbs.py
@@ -12,7 +12,10 @@ Note: Reflexive verbs don't include pronouns (e.g. afeitarse conjugates to
       'afeito' instead of 'me afeito')
 """
 
-import sys
+import sys # used for argv and stderr
+import codecs # used to read utf8 file
+import io # used to write windows line endings to output file
+from urllib import urlencode
 from urllib2 import urlopen
 from bs4 import BeautifulSoup
 
@@ -31,9 +34,8 @@ params = {
     'rb3': 'no',
     'rb2': 'ra'
 }
-req_data = '&'.join([x + '=' + y for x,y in params.iteritems()])
-bad_words = {'oír': 'oir', 'reírse': 'reirse', 'sonreírse': 'sonreirse'}
-# encodings from http://www.tutorialspoint.com/html/html_url_encoding.htm
+# equivalent: req_data = '&'.join([x + '=' + y for x,y in params.iteritems()])
+req_data = urlencode(params)
 
 def extract_conjugations(html):
     soup = BeautifulSoup(html)
@@ -45,13 +47,19 @@ def extract_conjugations(html):
             conj_dict[tense] = [conj for conj in gen]
     return conj_dict
 
+# encodings from http://www.tutorialspoint.com/html/html_url_encoding.htm
+accent_map = {u'á':u'%e1', u'é':u'%e9', u'í':u'%ed', u'ó':u'%f3', u'ú':u'%fa', u'ü':u'%fc', u'ñ':u'%f1'}
 def encode_accents(word):
-    return word.replace('á', '%e1').replace('é', '%e9').replace('í', '%ed').replace('ó', '%f3').replace('ú', '%fa').replace('ü', '%fc').replace('ñ', '%f1')
+    return ''.join(accent_map.get(x, x) for x in word)
+accent_inv = {v:k for k,v in accent_map.items()}
+def decode_accents(word):
+    return ''.join(accent_inv.get(x, x) for x in word)
 
 def conjugations_for_word(word):
     # conjugation.org can't handle accented characters in request...
-    req_word = fix_bad_words(word)
-    req_word = encode_accents(req_word)
+    #req_word = fix_bad_words(word)
+    #req_word = urllib.quote_plus(req_word) # throws UnicodeWarning =(
+    req_word = encode_accents(word)
     data = '%s&word=%s' % (req_data, req_word)
     resp = None
     try:
@@ -64,44 +72,41 @@ def conjugations_for_word(word):
         if resp: resp.close()
 
 def conjugations_for_words(words):
-    conjs = {}
-    for word in words:
-        conjs[word] = conjugations_for_word(word)
-    return conjs
-    
+    return {word:conjugations_for_word(word) for word in words}
 
 def get_word_list(fin):
     words = []
-    with open(fin, 'r') as f:
+    with codecs.open(fin, 'r', 'utf8') as f:
         # ignore empty and commented lines
         words = [line.strip() for line in f \
-                 if len(line.strip()) > 2 and not line.startswith("//")]
+                 if len(line.strip()) > 0 and not line.startswith("//")]
     return words
     
+# words where conjugation.org doesn't recognize the correct verb spelling
+bad_words = {'oír': 'oir', 'reírse': 'reirse', 'sonreírse': 'sonreirse'}
+
 # fix_bad_ functions correct errors caused by conjugation.org
 def fix_bad_words(word):
-    if word == 'oír': word = 'oir'
-    if word == 'reírse': word = 'reirse'
-    if word == 'sonreírse': word = 'sonreirse'
-    return word
+    return bad_words.get(word, word)
 def fix_bad_conjugations(conj_dict):
     if 'cerrar' in conj_dict:
         conj_dict['cerrar']['PresentIndicative'] = [u'cierro',u'cierras',u'cierra',u'cerramos',u'cerráis',u'cierran']
         conj_dict['cerrar']['PresentSubjunctive'] = [u'cierre',u'cierres',u'cierre',u'cerremos',u'cerréis',u'cierren']
-        conj_dict['cerrar']['Indicative'] = [u'cierra',u'cierre',u'cerremos',u'cerrad',u'cierren']
+        conj_dict['cerrar']['Imperative'] = [u'cierra',u'cierre',u'cerremos',u'cerrad',u'cierren']
     return conj_dict
 
-# TODO: output to a file for parsing by actionscript... (json?)
 def main():
     assert len(sys.argv) == 2, "Expects 1 argument (file of verbs to conjugate)"
     words = get_word_list(sys.argv[1])
     conj_dict = conjugations_for_words(words)
     conj_dict = fix_bad_conjugations(conj_dict)
-    #print conj_dict
+
     import json
-    txt = json.dumps(conj_dict, sort_keys=True,
+    txt = json.dumps(conj_dict, sort_keys=True, ensure_ascii=False,
                      indent=4, separators=(',', ': '))
-    print txt
+    #print txt
+    with io.open('output', 'w', newline='\r\n') as f:
+        f.write(txt)
     
 
 
